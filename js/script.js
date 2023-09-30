1f954e8edae809f48353f0017715d0f9fe186c5f
Anonymus1145 <Anonymus1145@yahoo.com>
2023-09-30 21:25:00 +0300

    Done the first part of the game

--- js/script.js
+++ js/script.js
@@ -13,27 +13,53 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+
+    let playerScore = 0;
+    let computerScore = 0;
 
     if (playerSelection == computerSelection) {
         alert("We have a draw");
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         alert("Computer wins!");
+        computerScore++;
     } else if (playerSelection == "rock" && computerSelection == "scissor") {
-        alert("Computer wins!");
+        alert("Player wins!");
+        playerScore++;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         alert("Player wins!");
+        playerScore++;
     } else if (playerSelection == "paper" && computerSelection == "scissor") {
         alert("Computer wins!");
+        computerScore++;
     } else if (playerSelection == "scissor" && computerSelection == "paper") {
         alert("Player wins!");
+        playerScore++
     } else if (playerSelection == "scissor" && computerSelection == "rock") {
         alert("Computer wins!");
+        computerScore++;
     } else {
-        alert("Wrong input!");
+        alert("Please enter a valid choice: rock, paper, or scissors.");
     }
 }
 
-const playerSelection = prompt("Insert your choice").toLocaleLowerCase();
-const computerSelection = getComputerChoice();
-prompt(playRound(playerSelection, computerSelection));
+function game() {
+
+    let playerSelection;
+    let computerSelection;
+
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Insert your choice!").toLocaleLowerCase();
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+    if (playerScore > computerScore) {
+        alert("Player won the game!");
+    } else if (playerScore < computerScore) {
+        alert("Computer won the game!");
+    } else {
+        alert("Final result id draw!");
+    }
+}
+
+let winner = game();
