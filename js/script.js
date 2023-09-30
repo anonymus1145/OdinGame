7ea9a5dbbd352af34abb0f2e6595b6b577648208
Anonymus1145 <Anonymus1145@yahoo.com>
2023-09-30 16:02:57 +0300

    Add playRound function
    Create a function that returns a decision based on the input received from the user
    
    [Ticket: 3]

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
 function getComputerChoice() {
 
-    let number = Math.floor((Math.random() * 2) + 1);
+    let number = Math.floor(Math.random() * 3);
     let computerChoice;
     if (number == 0) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (number == 1) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else {
-        computerChoice = "Scissor";
+        computerChoice = "scissor";
     }
+
     return computerChoice;
 }
+
+function playRound (playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+        alert("We have a draw");
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        alert("Computer wins!");
+    } else if (playerSelection == "rock" && computerSelection == "scissor") {
+        alert("Computer wins!");
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        alert("Player wins!");
+    } else if (playerSelection == "paper" && computerSelection == "scissor") {
+        alert("Computer wins!");
+    } else if (playerSelection == "scissor" && computerSelection == "paper") {
+        alert("Player wins!");
+    } else if (playerSelection == "scissor" && computerSelection == "rock") {
+        alert("Computer wins!");
+    } else {
+        alert("Wrong input!");
+    }
+}
+
+const playerSelection = prompt("Insert your choice").toLocaleLowerCase();
+const computerSelection = getComputerChoice();
+prompt(playRound(playerSelection, computerSelection));
