814ebc2e2476802240edc9b8cdd654db00b3daa3
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-04 23:13:58 +0300

    UI change

--- js/script.js
+++ js/script.js
@@ -43,75 +43,18 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
 
-    let playerSelection;
-    let computerSelection;
+btnRock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+})
 
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Insert your choice!").toLocaleLowerCase();
-        computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-    }
-    if (playerScore > computerScore) {
-        alert("Player won the game!");
-    } else if (playerScore < computerScore) {
-        alert("Computer won the game!");
-    } else {
-        alert("Final result id draw!");
-    }
-}
+btnPaper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+})
 
-//Add elements to the container with DOM methods
-const container = document.querySelector('#container');
-
-const content = document.createElement("p");
-content.style.color = "red";
-content.textContent = "Hey I’m red!";
-
-const contentH3 = document.createElement("h3");
-contentH3.style.color = "blue";
-contentH3.textContent = 'Hey I’m blue!';
-
-//container.appendChild(content);
-//container.appendChild(contentH3);
-
-const contentDiv = document.createElement("div");
-contentDiv.setAttribute('style', 'background: pink; border: black 1px solid');
-//Add elements to the div
-const divH1 = document.createElement("h1");
-divH1.textContent = "I am a div";
-const divP = document.createElement("p");
-divP.textContent = "Me too!";
-contentDiv.appendChild(divH1);
-contentDiv.appendChild(divP);
-
-//container.appendChild(contentDiv);
-
-
-//Add elements to the ul with DOM methods
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-    const myItem = input.value;
-    input.value = '';
-
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
-
-    listItem.appendChild(listText);
-    listText.textContent = myItem;
-    listText.appendChild(listBtn);
-    listBtn.textContent = 'Delete';
-
-    list.appendChild(listItem);
-
-    listBtn.addEventListener('click', () => {
-        list.removeChild(listItem);
-    });
-
-    input.focus();
-});
+btnScissors.addEventListener("click", () => {
+    playRound("scissor", getComputerChoice());
+})
