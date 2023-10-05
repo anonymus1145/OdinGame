86a73d2c1a5d8e16eeed71d17e46b23d9ecb390c
Anonymus1145 <Anonymus1145@yahoo.com>
2023-10-05 17:17:12 +0300

    Final UI

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Get the computer choice
+
 function getComputerChoice() {
 
     let number = Math.floor(Math.random() * 3);
@@ -13,48 +15,123 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
+//Play a round
 
-    let playerScore = 0;
-    let computerScore = 0;
+function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == computerSelection) {
         alert("We have a draw");
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        alert("Computer wins!");
-        computerScore++;
+        return false;
     } else if (playerSelection == "rock" && computerSelection == "scissor") {
-        alert("Player wins!");
-        playerScore++;
+        return true;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        alert("Player wins!");
-        playerScore++;
+        return true;
     } else if (playerSelection == "paper" && computerSelection == "scissor") {
-        alert("Computer wins!");
-        computerScore++;
+        return false;
     } else if (playerSelection == "scissor" && computerSelection == "paper") {
-        alert("Player wins!");
-        playerScore++
+        return true;
     } else if (playerSelection == "scissor" && computerSelection == "rock") {
-        alert("Computer wins!");
-        computerScore++;
-    } else {
-        alert("Please enter a valid choice: rock, paper, or scissors.");
+        return false;
     }
 }
+
+//Display the score and the winner
 
 const btnRock = document.querySelector("#rock");
 const btnPaper = document.querySelector("#paper");
 const btnScissors = document.querySelector("#scissors");
+let playerScore = 0;
+let computerScore = 0;
+let result;
 
 btnRock.addEventListener("click", () => {
-    playRound("rock", getComputerChoice());
+
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore > computerScore) {
+            alert("Game Over");
+            alert("Player win!");
+        } else {
+            alert("Game Over");
+            alert("Computer win!");
+        }
+        playerScore = 0;
+        computerScore = 0;
+        document.getElementById("playerScore").innerHTML = playerScore;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    } else {
+        result = playRound("rock", getComputerChoice());
+        if (result == true) {
+            playerScore++;
+            document.getElementById("playerScore").innerHTML = playerScore;
+            document.getElementById("decision").innerHTML = "You win!";
+            document.getElementById("decision").style.color = "green";
+        } else if (result == false) {
+            computerScore++;
+            document.getElementById("computerScore").innerHTML = computerScore;
+            document.getElementById("decision").innerHTML = "You lose!";
+            document.getElementById("decision").style.color = "red";
+        }
+    }
 })
 
+
 btnPaper.addEventListener("click", () => {
-    playRound("paper", getComputerChoice());
+
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore > computerScore) {
+            alert("Game Over");
+            alert("Player win!");
+        } else {
+            alert("Game Over");
+            alert("Computer win!");
+        }
+        playerScore = 0;
+        computerScore = 0;
+        document.getElementById("playerScore").innerHTML = playerScore;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    } else {
+        result = playRound("paper", getComputerChoice());
+        if (result == true) {
+            playerScore++;
+            document.getElementById("playerScore").innerHTML = playerScore;
+            document.getElementById("decision").innerHTML = "You win!";
+            document.getElementById("decision").style.color = "green";
+        } else if (result == false) {
+            computerScore++;
+            document.getElementById("computerScore").innerHTML = computerScore;
+            document.getElementById("decision").innerHTML = "You lose!";
+            document.getElementById("decision").style.color = "red";
+        }
+    }
 })
 
 btnScissors.addEventListener("click", () => {
-    playRound("scissor", getComputerChoice());
+
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore > computerScore) {
+            alert("Game Over");
+            alert("Player win!");
+        } else {
+            alert("Game Over");
+            alert("Computer win!");
+        }
+        playerScore = 0;
+        computerScore = 0;
+        document.getElementById("playerScore").innerHTML = playerScore;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    } else {
+        result = playRound("scissor", getComputerChoice());
+        if (result == true) {
+            playerScore++;
+            document.getElementById("playerScore").innerHTML = playerScore;
+            document.getElementById("decision").innerHTML = "You win!";
+            document.getElementById("decision").style.color = "green";
+        } else if (result == false) {
+            computerScore++;
+            document.getElementById("computerScore").innerHTML = computerScore;
+            document.getElementById("decision").innerHTML = "You lose!";
+            document.getElementById("decision").style.color = "red";
+        }
+    }
 })
